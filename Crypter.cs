91ee0e5d7bd23943b9715b1b129e1aa088296668
BolNick <BolNick09RUS@gmail.com>
2024-11-12 12:28:10 +0300

    Добавьте файлы проекта.

---
+++ Crypter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncCrypt
+{
+    internal class Crypter
+    {
+        public string OrigText { get; set; }
+        public string CryptedText { get; set; }
+
+        private string origFileName;
+        private string cryptedFileName;
+
+        public Crypter(string origFileName, string cryptedFileName)
+        {
+            this.origFileName = origFileName;
+            this.cryptedFileName = cryptedFileName;
+        }
+
+        public async Task ReadText()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(origFileName))
+                {
+                    OrigText = await reader.ReadToEndAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка чтения файла: " + ex.Message);
+            }
+        }
+        public void EncryptText()
+        {
+            byte[] origBytes = Encoding.UTF8.GetBytes(OrigText);
+            byte[] cryptedBytes = new byte[origBytes.Length];
+
+            for (int i = 0; i < origBytes.Length; i++)
+                cryptedBytes[i] = (byte)(origBytes[i] + 1);
+
+
+            CryptedText = Encoding.UTF8.GetString(cryptedBytes);
+        }
+        public async Task WriteCryptedText()
+        {
+            StreamWriter writer = new StreamWriter(cryptedFileName, false, Encoding.UTF8);
+            await writer.WriteAsync(CryptedText);
+            writer.Close();
+        }
+
+        public async Task EncryptFile()
+        {
+            await ReadText();
+            EncryptText();
+            await WriteCryptedText();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace AsyncCrypt
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            String[] OrigFileNames =
+            {
+                "file1.txt",
+                "file2.txt",
+                "file3.txt",
+                "file4.txt"
+            };
+            String[] CryptedFileNames =
+            {
+                "cfile1.txt",
+                "cfile2.txt",
+                "cfile3.txt",
+                "cfile4.txt"
+            };
+            Task[] tasks = new Task[4];
+
+            Crypter[] crypters = 
+              {
+                new Crypter(OrigFileNames[0], CryptedFileNames[0]),
+                new Crypter(OrigFileNames[1], CryptedFileNames[1]),
+                new Crypter(OrigFileNames[2], CryptedFileNames[2]),
+                new Crypter(OrigFileNames[3], CryptedFileNames[3])
+            };
+            for (int i = 0; i < OrigFileNames.Length; i++)
+            {
+                int index = i;
+                await crypters[index].EncryptFile();
+            }
+
+            Console.WriteLine("Файлы зашифрованы");
+
+
+        }
+    }
+}
